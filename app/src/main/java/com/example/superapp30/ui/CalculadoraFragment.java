2acbfb8f5875fe2332b05ec74a4e2f31e58419af
Gabriel Dümes <gabriel.dumes@hotmail.com>
2021-03-23 20:35:34 -0300

    Mini app "Calculadora" finalizado

--- app/src/main/java/com/example/superapp30/ui/CalculadoraFragment.java
+++ app/src/main/java/com/example/superapp30/ui/CalculadoraFragment.java
@@ -7,60 +7,81 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.superapp30.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalculadoraFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CalculadoraFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText editPrimeiroValor, editSegundoValor;
+    private TextView textSelecioneOperacao, textResultado;
+    private RadioGroup radioGroupOperacao;
+    private Button buttonCalcular;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CalculadoraFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalculadoraFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CalculadoraFragment newInstance(String param1, String param2) {
-        CalculadoraFragment fragment = new CalculadoraFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculadora, container, false);
+        View view = inflater.inflate(R.layout.fragment_calculadora, container, false);
+
+        editPrimeiroValor = view.findViewById(R.id.editPrimeiroValor);
+        editSegundoValor = view.findViewById(R.id.editSegundoValor);
+        textSelecioneOperacao = view.findViewById(R.id.textSelecioneOperacao);
+        textResultado = view.findViewById(R.id.textResultadoCalc);
+        radioGroupOperacao = view.findViewById(R.id.radioGroupOperacao);
+        buttonCalcular = view.findViewById(R.id.buttonCalcular);
+
+        textResultado.setVisibility(View.INVISIBLE);
+
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textResultado.setVisibility(View.INVISIBLE);
+                textSelecioneOperacao.setTextColor(getResources().getColor(android.R.color.tab_indicator_text));
+
+                String valor1String = editPrimeiroValor.getText().toString();
+                String valor2String = editSegundoValor.getText().toString();
+
+                if (valor1String.equals("") || valor1String.equals(".")) {
+                    Toast.makeText(getActivity(), "Primeiro valor INVÁLIDO!", Toast.LENGTH_SHORT).show();
+                } else if (valor2String.equals("") || valor2String.equals(".")) {
+                    Toast.makeText(getActivity(), "Segundo valor INVÁLIDO!", Toast.LENGTH_SHORT).show();
+                } else {
+                    double valor1 = Double.parseDouble(valor1String);
+                    double valor2 = Double.parseDouble(valor2String);
+                    Double resultado = null;
+
+                    switch (radioGroupOperacao.getCheckedRadioButtonId()) {
+                        case R.id.radioSomar:
+                            resultado = valor1 + valor2;
+                            break;
+                        case R.id.radioSubtrair:
+                            resultado = valor1 - valor2;
+                            break;
+                        case R.id.radioMultiplicar:
+                            resultado = valor1 * valor2;
+                            break;
+                        case R.id.radioDividir:
+                            resultado = valor1 / valor2;
+                            break;
+                        case R.id.radioMedia:
+                            resultado = (valor1 + valor2) / 2;
+                            break;
+                        default:
+                            textSelecioneOperacao.setTextColor(getResources().getColor(R.color.design_default_color_error));
+                            Toast.makeText(getActivity(), "Selecione uma operação!", Toast.LENGTH_SHORT).show();
+                    }
+                    if (resultado != null) {
+                        textResultado.setVisibility(View.VISIBLE);
+                        textResultado.setText(String.format("%.2f", resultado));
+                    }
+                }
+            }
+        });
+
+        return view;
     }
 }
