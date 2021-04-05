05a8c939808c18ffa856176fb01cd4469c618d16
Gabriel Dümes <gabriel.dumes@hotmail.com>
2021-04-05 19:49:50 -0300

    Abas mini app "Media"

---
+++ app/src/main/java/com/example/superapp30/ui/abas_media/VideoFragment.java
@@ -0,0 +1,20 @@
+package com.example.superapp30.ui.abas_media;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.superapp30.R;
+
+public class VideoFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video, container, false);
+    }
+}
